6e7c9cb17c6352809eceddd692ad65781d8f1cc0
Josue Baez <josue.baez@coderhouse.com>
2023-03-30 19:50:51 -0300

    ADD, form group definido

--- src/app/estudiantes/estudiantes.component.ts
+++ src/app/estudiantes/estudiantes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-estudiantes',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./estudiantes.component.css']
 })
 export class EstudiantesComponent {
+
+  estudianteForm: FormGroup = new FormGroup({});
 
 }
--- src/app/estudiantes/estudiantes.module.ts
+++ src/app/estudiantes/estudiantes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EstudiantesComponent } from './estudiantes.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { EstudiantesComponent } from './estudiantes.component';
     EstudiantesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
   ],
   exports: [
     EstudiantesComponent
